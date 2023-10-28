6b2b6a6fa1cafb922b4842d414c2ce28489aee70
AntonioRomero05 <20200766@uthh.edu.mx>
2023-10-27 18:51:25 -0600

    correcciones para test yml

--- test/form.js
+++ test/form.js
@@ -3,11 +3,11 @@ import { Selector } from 'testcafe';
 fixture('Test Cafe smoke test')
   .page('https://figuras-angular-mc0atadkn-jose-antonio-s-projects.vercel.app/circulo');
 
-test('Verificar la existencia de un input', async (t) => {
-  // Definir un selector para el elemento input
-  const inputElement = Selector('input');
+test('Verificar título de la página', async (t) => {
+  // Definir un selector para el título de la página
+  const pageTitle = Selector('title');
 
-  // Realizar la aserción para verificar si el elemento input existe
+  // Realizar la aserción
   await t
-    .expect(inputElement.exists).ok('El elemento input debería existir en la página.');
+    .expect(pageTitle.innerText).eql('Calculadorafigurass');
 });

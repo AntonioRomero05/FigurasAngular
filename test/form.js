b6c9618cc4fd5175f20cd79d6b11cc01aa5bb9b6
AntonioRomero05 <20200766@uthh.edu.mx>
2023-10-27 18:32:06 -0600

    correcion del yml

--- test/form.js
+++ test/form.js
@@ -1,10 +1,10 @@
 import { Selector } from 'testcafe';
 
 fixture `TestCafe Smoke Test`
-    .page `https://figuras-angular-mc0atadkn-jose-antonio-s-projects.vercel.app/inicio`; // Asegúrate de reemplazar con tu URL local
+    .page `https://figuras-angular-mc0atadkn-jose-antonio-s-projects.vercel.app/circulo`; // Asegúrate de reemplazar con tu URL local
 
 test('TestCafe is Working Properly', async t => {
     // Agrega aquí tu lógica de prueba
     await t
-        .expect(Selector('form').exists).ok();
+        .expect(Selector('#radio_title').innerText).eql("Radio");
 });
